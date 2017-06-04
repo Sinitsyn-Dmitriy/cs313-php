55b5cd94720f58d3f0e4aad3a1eea53305608563
Dmitriy Sinitsyn <2992274@gmail.com>
2017-06-03 20:24:24 -0600

    new page

--- web/info.php
+++ web/info.php
@@ -129,7 +129,8 @@ function drawChart1() {
     ['Date', 'Exercises', 'Food'],
     ['2017-05-18',  1000,      400],
     ['2005',  1170,      460],
-<?php  //foreach ($food as $value) { echo "['" . $value[date] .  $value[meal] . ", 0, " . $value[calories] . "],"; }  ?>
+// ['2017-05-20Dinner, 0, 420],['2017-05-19Snack, 0, 40],['2017-05-18Lunch, 0, 150],['2017-05-26Snack, 0, 200],['2017-05-24Lunch, 0, 100],['2017-05-24Dinner, 0, 300],['2017-05-24Dinner, 0, 300],
+<?php  foreach ($food as $value) { echo "['" . $value[date] .   $value[meal] . "', '0', '" . $value[calories] . "'],"; }  ?>
 
     ['2017',  1030,      540]
   ]);
